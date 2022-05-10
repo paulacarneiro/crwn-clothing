e491624edc78de7c4151f2113cea6205a3fb6781
Carneiro <paulacarneiro215@gmail.com>
2022-05-10 17:38:47 +0100

    Fix sign out issue

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -96,6 +96,7 @@ export function* userSagas() {
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(onSignUpStart),
-    call(onSignUpSuccess)
+    call(onSignUpSuccess),
+    call(onSignOutStart)
   ]));
 }
