28fa78132b160377a1deef2452240e29dfb241a3
Carneiro <paulacarneiro215@gmail.com>
2022-05-10 17:43:11 +0100

    Add stripe payment and payment form

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from './checkout.styles';
 
@@ -34,6 +35,7 @@ const Checkout = () => {
           ))
         }
       <Total>Total: &euro; {cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 }
