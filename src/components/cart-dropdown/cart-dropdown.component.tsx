30246334187c651f4eb9ba9fc1021f71d59198a1
Carneiro <paulacarneiro215@gmail.com>
2022-06-03 13:20:34 +0100

    Close cart dropdown on outside click

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,27 +1,41 @@
-import { useCallback } from 'react';
-import { useSelector } from 'react-redux';
+import { useCallback, useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
-import {
-  CartDropdownContainer,
-  EmpyMessage,
-  CartItems,
-} from './cart-dropdown.styles';
+import { CartDropdownContainer, EmpyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
+  const ref = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch();
 
   const goToCheckoutHandler = useCallback(() => {
+    dispatch(setIsCartOpen(false));
     navigate('/checkout');
   }, []);
 
+  const handleClick = (e: Event) => {
+    if (ref.current && !ref.current.contains(e.target as Element)) {
+      dispatch(setIsCartOpen(false));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer ref={ref}>
       <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
